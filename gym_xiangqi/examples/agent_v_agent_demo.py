5ed07c9f647b3389db6a9e07ebe7d705d4b218e4
Qingnan Duan <duanqn_own_1@yeah.net>
2021-12-31 10:47:33 +0800

    Display-related changes

--- gym_xiangqi/examples/agent_v_agent_demo.py
+++ gym_xiangqi/examples/agent_v_agent_demo.py
@@ -1,5 +1,5 @@
 from gym_xiangqi.agents import RandomAgent
-from gym_xiangqi.constants import ALLY, PIECE_ID_TO_NAME
+from gym_xiangqi.constants import ALLY, PIECE_ID_TO_NAME_CHS
 from gym_xiangqi.utils import action_space_to_move
 
 import gym
@@ -13,23 +13,29 @@ def main():
 
     done = False
     round = 0
+    env.reset()
     while not done:
         # Add a slight delay to properly visualize the game.
-        time.sleep(1)
+        time.sleep(0.1)
 
         action = agent.move(env)
         _, reward, done, _ = env.step(action)
         turn = "Ally" if env.turn == ALLY else "Enemy"
         move = action_space_to_move(action)
-        piece = PIECE_ID_TO_NAME[move[0]]
+        piece = PIECE_ID_TO_NAME_CHS[0 if env.turn == ALLY else 1][move[0]]
 
         print(f"Round: {round}")
         print(f"{turn} made the move {piece} from {move[1]} to {move[2]}.")
         print(f"Reward: {reward}")
+        if env.jiangjun:
+            print("将军！")
         print("================")
 
         round += 1
         env.render()
+
+    print("Game finished!")
+    input()
     env.close()
 
 
